2d6e0db1a2411d0d3282c8220ec94242ae1266a9
Joseph Adediji <josylado@gmail.com>
2019-08-31 13:20:23 +0300

    create the class User

--- user_class.py
+++ user_class.py
@@ -1,3 +1,11 @@
 import pyperclip #importing pyperclip for copying to clipboard
 
+#Built by Joseph Adediji 
+#Edit at your own risk! 
+
+
+class User:
+	'''
+	Class to create user accounts and save their details
+	'''
 
