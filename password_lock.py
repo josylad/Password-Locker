f9ccf0b53022a41ec125bff89e94648043236008
Joseph Adediji <josylado@gmail.com>
2019-08-31 13:48:47 +0300

    create password lock file

---
+++ password_lock.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+import pyperclip  #importing pyperclip for copying to clipboard
+from user_class import User  #imporing user class
+from credential_class import Credential #importing credential class
+
+#Built by Joseph Adediji  
+#Edit at your own risk! 
+
+
