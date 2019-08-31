6b0fe871001cd77c50ba57e84389b2b3c207720c
Joseph Adediji <josylado@gmail.com>
2019-08-31 13:21:01 +0300

    Method to define properties of each user

--- user_class.py
+++ user_class.py
@@ -9,3 +9,15 @@ class User:
 	Class to create user accounts and save their details
 	'''
 
+
+	def __init__(self,first_name,last_name,password):
+
+		'''
+		Method to define the properties for each users.
+		'''
+
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
+
+	
