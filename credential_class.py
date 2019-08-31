f4d9969ecab7715e9d5bd93a6ea896aa3991305e
Joseph Adediji <josylado@gmail.com>
2019-08-31 13:24:00 +0300

    Create credentials class file

---
+++ credential_class.py
@@ -0,0 +1,5 @@
+import pyperclip #importing pyperclip for copying to clipboard
+from user_class import User #importing user class 
+import random #import random variable generator
+import string  #import string constants
+
