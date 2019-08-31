738b971da563753fa78541f6411b622b6b8fa43d
Joseph Adediji <josylado@gmail.com>
2019-08-31 13:24:41 +0300

    Create the class for the credentials

--- credential_class.py
+++ credential_class.py
@@ -3,3 +3,10 @@ from user_class import User #importing user class
 import random #import random variable generator
 import string  #import string constants
 
+#Built by Joseph Adediji 
+#Edit at your own risk! 
+
+class Credential:
+	'''
+	Class to create  account credentials, generate new passwords and save user information
+	'''
