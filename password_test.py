c33a2b0b434d0eee4f52ef3a05004a7744fb1cca
Joseph Adediji <josylado@gmail.com>
2019-09-01 11:20:00 +0300

    Add setup method to run before each case

--- password_test.py
+++ password_test.py
@@ -14,4 +14,44 @@ class TestContact(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
     
-  
+    def setUp(self):
+        '''
+        Setup method to run before each test case 
+        ''' 
+        self.new_user = User('Joseph', 'Adediji', '123456')
+    
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Joseph")
+        self.assertEqual(self.new_user.last_name,"Adediji")
+        self.assertEqual(self.new_user.password,"123456")
+        
+    def test_save_user(self):
+        '''
+		Test to check if the new users info is saved into the users list
+		'''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
+        
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.users_list = []
+
+        
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines the test cases for the the credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+		Setup method to run before each test case
+		'''
+        self.new_credential = Credential('Joseph','FB.com','Josylad','123456')
