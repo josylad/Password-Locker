b7c05a39875f3b4eb6b7b39c4748916e84c679d2
Joseph Adediji <josylado@gmail.com>
2019-09-01 11:19:19 +0300

    Add testcase class

--- password_test.py
+++ password_test.py
@@ -1,3 +1,17 @@
+import unittest
+import pyperclip  #importing pyperclip for copying to clipboard
+from user_class import User  #imporing user class
+from credential_class import Credential #importing credential class
 
 #Built by Joseph Adediji 
-#Edit at your own risk! 
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines the test cases for the the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    
+  
