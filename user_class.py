b1099a6a5ad6afcf4c478686fde5711382016fbd
Joseph Adediji <josylado@gmail.com>
2019-09-01 21:42:36 +0300

    Optimize code for readability and add comments

--- user_class.py
+++ user_class.py
@@ -1,9 +1,6 @@
 import pyperclip #importing pyperclip for copying to clipboard
 
 #Built by Joseph Adediji 
-#Edit at your own risk! 
-
-
 class User:
 	'''
 	Class to create user accounts and save their details
